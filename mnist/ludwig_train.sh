4271fea6235fddefcb7c0f07690cb7a82a2880a9
Jim Thompson <jimthompson5802@aol.com>
2019-10-19 17:39:20 -0400

    feat: converted to updated docker image

--- mnist/ludwig_train.sh
+++ mnist/ludwig_train.sh
@@ -3,7 +3,8 @@
 
 docker run \
   -v ${PWD}:/opt/project \
-  ludwig experiment \
+  ludwig_cpu \
+   ludwig experiment \
   --data_train_csv /opt/project/data/mnist_dataset_training.csv \
   --data_test_csv /opt/project/data/mnist_dataset_testing.csv \
   --model_definition_file /opt/project/model_definition.yaml
