66607090b9e8c1dea7729c2b78f15b4795cb4d05
Jim Thompson <jimthompson5802@aol.com>
2019-10-22 22:13:49 -0400

    feat: modified to do train/valid/test split of training data

--- mnist/ludwig_cli.sh
+++ mnist/ludwig_cli.sh
@@ -12,6 +12,7 @@ docker run \
   -v ${PWD}:/opt/project \
   ludwig_${image_suffix} \
    ludwig experiment \
-  --data_train_csv /opt/project/data/mnist_dataset_training.csv \
-  --data_test_csv /opt/project/data/mnist_dataset_testing.csv \
-  --model_definition_file /opt/project/model_definition.yaml
+     --model_definition_file /opt/project/model_definition.yaml \
+     --data_csv /opt/project/data/mnist_dataset_training.csv
+#  --data_test_csv /opt/project/data/mnist_dataset_testing.csv \
+
