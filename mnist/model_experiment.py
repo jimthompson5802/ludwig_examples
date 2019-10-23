2081a840bfa93be9dc3b348d6b0fabb9755bf156
Jim Thompson <jimthompson5802@aol.com>
2019-10-22 20:10:55 -0400

    chore: code clean-up

--- mnist/model_experiment.py
+++ mnist/model_experiment.py
@@ -5,7 +5,7 @@ import logging
 
 
 # train a model
-model_definition = {}
+model_definition = {...}
 model = LudwigModel(model_definition, model_definition_file='./model_definition.yaml', logging_level=logging.INFO)
 train_stats = model.train(data_train_csv='./data/mnist_dataset_training.csv',
                           data_test_csv='./data/mnist_dataset_testing.csv')
