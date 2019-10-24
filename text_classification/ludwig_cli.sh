90ad265c9a45dd45a1b3785b52ca42fd867d2f86
Jim Thompson <jimthompson5802@aol.com>
2019-10-23 21:34:22 -0400

    feat: initial check-in text classification

---
+++ text_classification/ludwig_cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xe
+
+#
+# Script to run a Ludwig experiment via command line
+#
+
+image_suffix=${1:-tf_cpu}
+
+docker run \
+  --rm \
+  -v ${PWD}:/opt/project \
+  ludwig_${image_suffix} \
+   ludwig experiment \
+      --data_csv ./data/reuters-allcats.csv \
+      --model_definition_file ./model_definition.yaml
