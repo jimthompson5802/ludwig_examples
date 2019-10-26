bf338f4387ccbcd803b149f4fd06e91d1219e88a
Jim Thompson <jimthompson5802@aol.com>
2019-10-26 15:59:45 -0400

    chore: clean-up cli example to remove unneeded parameter

--- mnist/ludwig_cli.sh
+++ mnist/ludwig_cli.sh
@@ -4,7 +4,7 @@ image_suffix=${1:-tf_cpu}
 
 ../bin/run_batch_cli ${image_suffix} \
    ludwig experiment \
+    --experiment_name mnist_cli_experiment \
      --model_definition_file /opt/project/model_definition.yaml \
-     --data_train_csv /opt/project/data/mnist_dataset_training.csv \
-     --data_test_csv /opt/project/data/mnist_dataset_testing.csv \
+     --data_csv /opt/project/data/mnist_dataset_training.csv
 
