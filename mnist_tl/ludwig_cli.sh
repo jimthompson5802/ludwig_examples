db679361d343500e2824eadfab6ed2f1f4343b40
Jim Thompson <jimthompson5802@aol.com>
2019-11-03 16:01:07 -0500

    feat: initial check-in

---
+++ mnist_tl/ludwig_cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+image_suffix=${1:-tf_cpu}
+
+../bin/run_batch_cli ${image_suffix} \
+   ludwig experiment \
+    --experiment_name mnist_tl_cli_experiment \
+     --model_definition_file /opt/project/model_definition.yaml \
+     --data_train_csv /opt/project/data/odd/mnist_dataset_training.csv \
+     --data_test_csv /opt/project/data/odd/mnist_dataset_testing.csv
+
+
