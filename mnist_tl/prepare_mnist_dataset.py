2a02b6394283a5470c56fc72e9f736285f264339
Jim Thompson <jimthompson5802@aol.com>
2019-11-03 15:12:08 -0500

    feat: initial check-in for even/odd data preparation for training

---
+++ mnist_tl/prepare_mnist_dataset.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import os.path
+import shutil
+
+
+# ## start out with fresh data
+
+# In[2]:
+
+
+try:
+    shutil.rmtree('./results')
+except:
+    pass
+
+
+os.chdir('./data')
+for digit_type in ['even', 'odd']:
+    for name in ['training', 'testing']:
+        file_name = os.path.join(digit_type,'mnist_dataset_{}.csv'.format(name))
+        with open(file_name, 'w') as output_file:
+            print('=== creating {} dataset ==='.format(file_name))
+            output_file.write('image_path,label\n')
+            digits = os.listdir(os.path.join(digit_type, name))
+            for i in digits:
+                path = '{}/{}/{}'.format(digit_type, name, i)
+                for file in os.listdir(path):
+                    if file.endswith(".png"):
+                        output_file.write('{},{}\n'.format(os.path.join(path, file), str(i)))
+
+
+# In[ ]:
+print("Finished data preparation.")
+
+
+
+
