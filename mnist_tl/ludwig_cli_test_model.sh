d47d39bc39feec802b472762ace2650ee78d6ebe
Jim Thompson <jimthompson5802@aol.com>
2019-11-03 19:40:27 -0500

    feat: updated naming to match function

--- mnist_tl/ludwig_cli_test_model.sh
+++ mnist_tl/ludwig_cli_test_model.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 image_suffix=${1:-tf_cpu}
+model_digit=${2:-odd}
 
 ../bin/run_batch_cli ${image_suffix} \
    ludwig test \
-     --model_path results/mnist_tl_cli_experiment_run/model \
-     --output_directory results_test_odd_model_odds_data \
+     --model_path results/mnist_${model_digit}_digits_run/model \
+     --output_directory results_test_odd_model_odd_data \
      --split full \
-     --data_csv data/odd/mnist_dataset_testing.csv
+     --logging_level error \
+     --data_csv data/odd/mnist_dataset_testing.csv 2>/dev/null
+
+echo -e "\nAccuracy for ${model_digit} model on odd digits: " \
+  "$(jq .combined.accuracy results_test_odd_model_odd_data/test_statistics.json)\n"
 
 
 ../bin/run_batch_cli ${image_suffix} \
    ludwig test \
-     --model_path results/mnist_tl_cli_experiment_run/model \
+     --model_path results/mnist_${model_digit}_digits_run/model \
      --output_directory results_test_odd_model_even_data \
      --split full \
-     --data_csv data/even/mnist_dataset_testing.csv
+     --logging_level error \
+     --data_csv data/even/mnist_dataset_testing.csv 2>/dev/null
 
+echo -e "\nAccuracy for ${model_digit} model on even digits: " \
+  "$(jq .combined.accuracy results_test_odd_model_even_data/test_statistics.json)\n"
--- mnist_tl/ludwig_cli_train_odd_digits.sh
+++ mnist_tl/ludwig_cli_train_odd_digits.sh
@@ -4,7 +4,7 @@ image_suffix=${1:-tf_cpu}
 
 ../bin/run_batch_cli ${image_suffix} \
    ludwig train \
-    --experiment_name mnist_tl_cli_experiment \
+    --experiment_name mnist_odd_digits \
     --random_seed 1313 \
      --model_definition_file /opt/project/model_definition.yaml \
      --data_csv /opt/project/data/odd/mnist_dataset_training.csv
